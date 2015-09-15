564544fbe3210a871b4c22db0c63526ac97c6e4e
Frank Duan <qfduan@hotmail.com>
2015-09-15 17:04:37 -0400

    Revert "back to rails 3.2.x fix before_"
    This reverts commit 87e6b1da91782d911f01317c4047fe3382e0fc2f.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   before_filter :authenticate_user!
-  before_filter :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
 
   def index
     @users = User.all
